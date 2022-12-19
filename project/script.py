b9933fd2b2ea44846f0793c8ac6aa66169dc3a8d
pawelpiotrowskiwsb <91828950+pawelpiotrowskiwsb@users.noreply.github.com>
2022-12-19 18:43:25 +0100

    update

--- project/script.py
+++ project/script.py
@@ -4,6 +4,5 @@ import os
 import csv
 files = os.path.join("example*.csv")
 files = glob.glob(files)
-print("Resultant CSV after joining all CSV files at a particular location...")
 df = pd.concat(map(pd.read_csv, files), ignore_index=True)
 df.to_csv('Combined_files.csv')
